4544898ca0c43707db53b01299c2093cbabd3086
Vishal-Techy <vishalchinns1650@gmail.com>
2022-09-23 04:28:07 +0530

    completed IOT Dashboard - liveMedicalRecord

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,4 @@
+import React from 'react'
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
@@ -61,7 +62,7 @@ export default function Home() {
 
      </div>
             
-       </div>
+    </div>
 
 
 
